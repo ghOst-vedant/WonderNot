dd52a10179c7a4bd3dcad5763767dbe36df19a60
ghOst-vedant <gaikwadvedant30@gmail.com>
2024-04-29 19:12:28 +0530

    final

--- socket/index.js
+++ socket/index.js
@@ -7,7 +7,7 @@ const server = http.createServer();
 
 const io = new socketIoServer(server, {
   cors: {
-    origin: "https://wondernot.vercel.app/",
+    origin: ["https://wondernot.vercel.app/", "http://172.20.10.2:5173/"],
     methods: ["*"],
   },
 });
